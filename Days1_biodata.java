9a1e2e087309011a1853d7d57fc122240b6f34fb
Asdinisahabuddin <147237150+Asdinisahabuddin@users.noreply.github.com>
2023-10-09 18:05:44 +0800

    Add files via upload
    day1coding

---
+++ Days1_biodata.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.code;
+
+/**
+ *
+ * @author rahma
+ */
+public class Days1_biodata {
+
+    public static void main(String[] args) {
+        String nama = "Asdini Sahabuddin";
+        String nim = "D0222508";
+        String kelas = "Informatika E";
+        int angkatan = 2022;
+        String alamat = "Wonomulyo";
+        
+        System.out.println("Nama :"+ nama);
+        System.out.println("Nim :"+ nim);
+        System.out.println("Kelas :"+ kelas);
+        System.out.println("Angkatan :"+ angkatan);
+        System.out.println("Alamat :"+ alamat);
+    }
+}
