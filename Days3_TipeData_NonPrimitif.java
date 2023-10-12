6d4399de7e6399e1683cab36b8aa1716f80792a3
Asdinisahabuddin <147237150+Asdinisahabuddin@users.noreply.github.com>
2023-10-12 14:46:16 +0800

    Hari3

---
+++ Days3_TipeData_NonPrimitif.java
@@ -0,0 +1,18 @@
+package Asdini;
+
+public class Days3_TipeData_NonPrimitif {
+    public static void main(String[] args) {
+            Byte a = 127;
+            Short b = 32767;
+            Integer c = 25;
+            Long d = 100L;
+            Float e = 78f;
+            Double f = 1.12;
+            Character g = 'A';
+            Boolean h = true;
+            
+            
+            
+    }
+  
+}
